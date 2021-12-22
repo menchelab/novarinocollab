746c21a389b681c45b950fb5dd1f53c22d278040
daniel.malzl <daniel.malzl@clip-login-0.cbe.vbc.ac.at>
2021-12-22 08:49:58 +0100

    add first files

---
+++ copydata.sh
@@ -0,0 +1,1 @@
+cp /groups/vbcf-ngs/seq/novaseq/210812_A00700_0462_BHJK2WDRXY/Data/Intensities/BaseCalls/illumina2bam0.03_13-08-2021/*bam data
---
+++ download.sh
@@ -0,0 +1,2 @@
+wget -c --no-check-certificate --auth-no-challenge --user daniel.malzl --password 0M1n5O6p6Q! https://ngs.vbcf.ac.at/filemanager/byurl/tI1pFPpBJY-BPG61617-1021_0_R11754_20210707.tar.gz 
+wget -c --no-check-certificate --auth-no-challenge --user daniel.malzl --password 0M1n5O6p6Q! https://ngs.vbcf.ac.at/filemanager/byurl/2jMl7o1PXn-HJK2WDRXY_0_R11674_20210813.tar.gz 
