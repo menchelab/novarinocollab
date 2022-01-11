9475287d593b677b687784658467a4fa51c3db39
daniel.malzl <daniel.malzl@clip-login-0.cbe.vbc.ac.at>
2022-01-11 15:58:27 +0100

    add compass scripts

--- scripts/runNextflow.sh
+++ scripts/runNextflow.sh
@@ -3,6 +3,8 @@ nextflow run nf-core/rnaseq \
 	-profile cbe \
 	-w /scratch/daniel.malzl/work \
 	--input meta/samples.csv \
-	--genome mm10 \
+	--fasta data/GRCm39.primary_assembly.genome.fa \
+	--gtf data/gencode.vM28.annotation.gtf \\
+	--gencode \
 	--skip_deseq2_qc \
 	-resume
