03e2bd1d0ea3df86aa5ff48dd323ef17fd316972
dmalzl <UaF20091!>
2022-01-19 10:46:18 +0100

    add assembly download

--- scripts/download_annotations.sh
+++ scripts/download_annotations.sh
@@ -2,3 +2,5 @@ wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M28/genco
 gunzip data/gencode.vM28.annotation.gtf.gz
 cat data/gencode.vM28.annotation.gtf | grep -v '^#' > gencode.vM28.annotation.gtf
 mv gencode.vM28.annotation.gtf data
+wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M28/GRCm39.primary_assembly.genome.fa.gz -O data/GRCm39.primary_assembly.genome.fa.gz
+gunzip data/GRCm39.primary_assembly.genome.fa.gz
